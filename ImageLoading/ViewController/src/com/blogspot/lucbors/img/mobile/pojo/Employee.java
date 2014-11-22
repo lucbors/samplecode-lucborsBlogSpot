77c332de99ddc1c5afb6f32abe22ae9dbc255964
lucbors <lucbors@gmail.com>
2014-11-22 08:13:58 +0100

    This contains code for blogpost : http://www.lucbors.blogspot.nl/2014/11/maf-20-loading-images-in-background.html

---
+++ ImageLoading/ViewController/src/com/blogspot/lucbors/img/mobile/pojo/Employee.java
@@ -0,0 +1,117 @@
+package com.blogspot.lucbors.img.mobile.pojo;
+
+import oracle.adfmf.java.beans.PropertyChangeListener;
+import oracle.adfmf.java.beans.PropertyChangeSupport;
+
+
+public class Employee {
+    private int empId;
+    private String firstName;
+    private String lastName;
+    private boolean active = false;
+    private String image;
+
+
+    private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(int empId, String firstName, String lastName) {
+        this.empId = empId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    public void setEmpId(int empId) {
+        int oldEmpId = this.empId;
+        this.empId = empId;
+        propertyChangeSupport.firePropertyChange("empId", oldEmpId, empId);
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setFirstName(String firstName) {
+        String oldFirstName = this.firstName;
+        this.firstName = firstName;
+        propertyChangeSupport.firePropertyChange("firstName", oldFirstName, firstName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        String oldLastName = this.lastName;
+        this.lastName = lastName;
+        propertyChangeSupport.firePropertyChange("lastName", oldLastName, lastName);
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setImage(String image) {
+        String oldImage = this.image;
+        this.image = image;
+        propertyChangeSupport.firePropertyChange("image", oldImage, image);
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImageById(int id) {
+        Integer i = new Integer(id);
+        String image = i.toString() + ".png";
+        setImage(image);
+    }
+
+    private String getEmpImage(int id) {
+        String image = "nopic.png";
+
+        if (1 == 0) {
+
+            return image;
+        }
+
+        Integer i = new Integer(id);
+        if (i.intValue() < 158) {
+            image = i.toString() + ".png";
+        }
+        return image;
+    }
+
+    public void setActive(boolean active) {
+        boolean oldActive = this.active;
+        this.active = active;
+        propertyChangeSupport.firePropertyChange("active", oldActive, active);
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.addPropertyChangeListener(l);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener l) {
+        propertyChangeSupport.removePropertyChangeListener(l);
+    }
+
+    public void setPropertyChangeSupport(PropertyChangeSupport propertyChangeSupport) {
+        PropertyChangeSupport oldPropertyChangeSupport = this.propertyChangeSupport;
+        this.propertyChangeSupport = propertyChangeSupport;
+        propertyChangeSupport.firePropertyChange("propertyChangeSupport", oldPropertyChangeSupport,
+                                                 propertyChangeSupport);
+    }
+
+    public PropertyChangeSupport getPropertyChangeSupport() {
+        return propertyChangeSupport;
+    }
+}
